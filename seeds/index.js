f018416600717da60246a98ed3be6a44584ae594
amank5788 <88306289+amank5788@users.noreply.github.com>
2021-11-24 13:39:10 +0530

    try2

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Product = require('../models/product')
 
-mongoose.connect('mongodb://localhost:27017/productdb', {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/productdb', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
